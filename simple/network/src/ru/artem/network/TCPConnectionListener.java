7ce2eb353fa55f8b659c01ac83a0380a36e53686
Demedart <demedart@gmail.com>
2017-12-14 00:38:37 +0300

    delete

--- simple/network/src/ru/artem/network/TCPConnectionListener.java
+++
@@ -1,9 +0,0 @@
-package ru.artem.network;
-
-public interface TCPConnectionListener {
-
-    void onConnectionReady(TCPConnection tcpConnection);
-    void onReceiveString(TCPConnection tcpConnection, String value);
-    void onDisconnect(TCPConnection tcpConnection);
-    void onException(TCPConnection tcpConnection,Exception e);
-}
